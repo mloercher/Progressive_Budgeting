51b9d19635ee513f2d5cacd3565175ef6a10542a
Mark <markloercher@gmail.com>
2022-02-19 09:38:53 -0500

    fixed syntax error in deployment

--- server.js
+++ server.js
@@ -16,7 +16,7 @@ app.use(express.json());
 
 app.use(express.static("public"));
 
-mongoose.connect(MONGODB_URI,g {
+mongoose.connect(MONGODB_URI, {
   useNewUrlParser: true,
   useFindAndModify: false
 });
